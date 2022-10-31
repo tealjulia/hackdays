e2a41d1eb45219b21c081d5bc7736aa5c173563d
tealjulia <larson.teal@gmail.com>
2022-10-31 15:35:20 -0400

    get it reading from the document

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
+// use global chrome
+
+import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import { SidePanel } from "./components/SidePanel";
 
 function App() {
+  const [title, setTitle] = useState("");
+  useEffect(() => {
+    /**
+     * We can't use "chrome.runtime.sendMessage" for sending messages from React.
+     * For sending messages from React we need to specify which tab to send it to.
+     */
+    chrome.tabs &&
+      chrome.tabs.query(
+        {
+          active: true,
+          currentWindow: true,
+        },
+        (tabs) => {
+          /**
+           * Sends a single message to the content script(s) in the specified tab,
+           * with an optional callback to run when a response is sent back.
+           *
+           * The runtime.onMessage event is fired in each content script running
+           * in the specified tab for the current extension.
+           */
+          chrome.tabs.sendMessage(
+            tabs[0].id || 0,
+            { type: "GET_DOM" },
+            (response) => {
+              setTitle(response.title);
+            }
+          );
+        }
+      );
+  });
   return (
     <Container>
+      <h1>{title}</h1>
       <header className="App-header">
         <SidePanel />
       </header>
---
+++ src/content.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+
+const messagesFromReactAppListener = (msg, sender, sendResponse) => {
+  console.log("[content.js]. Message received", msg);
+
+  const response = {
+    title: document.title,
+    headlines: Array.from(document.getElementsByTagName < "h1" > "h1").map(
+      (h1) => h1.innerText
+    ),
+  };
+
+  console.log("[content.js]. Message response", response);
+
+  sendResponse(response);
+};
+
+/**
+ * Fired when a message is sent from either an extension process or a content script.
+ */
+chrome.runtime.onMessage.addListener(messagesFromReactAppListener);
+
+export {};
